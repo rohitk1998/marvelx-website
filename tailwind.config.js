9f16d003bc19ba99f48ddeb7cb87052783359f43
Rohit Kumar <rohit.kumar@antiersolutions.com>
2025-05-19 01:57:07 +0530

    done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",  // ← Very important
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
